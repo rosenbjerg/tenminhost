c02effa86d76a4d89809ba6bd178a8718560163e
Malte Rosenbjerg <rosenbjerg@users.noreply.github.com>
2019-04-02 00:06:43 +0200

    Removed old project and replaced with local new version

---
+++ FileInfoWrapper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+
+namespace tenminhost
+{
+    public class FileInfoWrapper
+    {
+        public string FullName { get; }
+        public string Name { get; }
+        public string Size { get; }
+        public int UploadedSecondsAgo { get; }
+        public string Uploaded { get; }
+
+        public FileInfoWrapper(FileInfo fileInfo)
+        {
+            FullName = fileInfo.Name;
+            Name = fileInfo.Name.Substring(7);
+            Size = Program.GetSizeString(fileInfo.Length);
+            Uploaded = Program.FormatTime(fileInfo.CreationTimeUtc);
+            UploadedSecondsAgo = (int) DateTime.UtcNow.Subtract(fileInfo.CreationTimeUtc).TotalSeconds;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,243 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+using Microsoft.AspNetCore.HttpOverrides;
+using Microsoft.Extensions.DependencyInjection;
+using NLog;
+using NLog.Fluent;
+using Red;
+using Red.HandlebarsRenderer;
+using Validation;
+
+namespace tenminhost
+{
+    class Program
+    {
+        const string UploadDirectory = "./uploads";
+        const int MaxFileSize = 367_001_600; // 350 MiB
+        const int DisplayKiBLimit = 1_024_000; // 1 MiB
+        const long MaxSpaceConsumption = 9_126_805_504; // 8.5 GiB
+
+        private static readonly char[] PossibleRandomCharacters =
+            "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
+
+
+        static async Task Main(string[] args)
+        {
+            var server = new RedHttpServer(5123, "public")
+            {
+                ConfigureApplication = app =>
+                {
+                    app.UseForwardedHeaders(new ForwardedHeadersOptions
+                    {
+                        ForwardedHeaders = ForwardedHeaders.XForwardedFor |
+                                           ForwardedHeaders.XForwardedProto
+                    });
+                },
+                ConfigureServices = serviceCollection =>
+                {
+                    serviceCollection.Configure<FormOptions>(formOptions =>
+                    {
+                        formOptions.ValueCountLimit = 1;
+                        formOptions.ValueLengthLimit = MaxFileSize;
+                        formOptions.MultipartBodyLengthLimit = MaxFileSize;
+                    });
+                }
+            };
+
+            
+            var uploadLogger = LogManager.GetLogger("Upload");
+            var maxSizeMiB = GetSizeString(MaxFileSize);
+
+            Directory.CreateDirectory(UploadDirectory);
+            var validForm = ValidatorBuilder.New().RequiresFile("file", file => file.Length < MaxFileSize).Build();
+
+            server.Get("/:filename", async (req, res) =>
+            {
+                var filename = HttpUtility.UrlDecode(req.Parameters["filename"]);
+                var filepath = Path.Combine(UploadDirectory, Path.GetFileName(filename));
+
+                if (File.Exists(filepath))
+                {
+                    var fileInfo = new FileInfo(filepath);
+                    await res.RenderTemplate("./templates/download.hbs", new FileInfoWrapper(fileInfo));
+                }
+                else
+                {
+                    await res.Redirect("/");
+                }
+            });
+
+            server.Post("/upload", async (req, res) =>
+            {
+                req.UnderlyingContext.Features.Get<IHttpMaxRequestBodySizeFeature>().MaxRequestBodySize = MaxFileSize;
+                IFormCollection form;
+                try
+                {
+                    form = await req.GetFormDataAsync();
+                }
+                catch (Exception)
+                {
+                    await res.SendString(
+                        $"Your file exceeds the maximum size ({maxSizeMiB})",
+                        status: HttpStatusCode.InsufficientStorage);
+                    return;
+                }
+                
+                if (validForm.Validate(form))
+                {
+                    var file = form.Files["file"];
+                    if (!CheckSpaceConsumption(file.Length))
+                    {
+                        await res.SendString(
+                            "Not enough space for your file right now, try again in a couple of minutes",
+                            status: HttpStatusCode.InsufficientStorage);
+                        uploadLogger.Log(LogLevel.Warn, "Maximum space consumption reached");
+                        return;
+                    }
+
+
+                    var randomChars = GenerateRandom(6);
+                    var filename = $"{randomChars}-{Path.GetFileName(file.FileName)}";
+                    var filepath = Path.Combine(UploadDirectory, filename);
+                    try
+                    {
+                        using (var outputStream = File.OpenWrite(filepath))
+                        {
+                            await file.CopyToAsync(outputStream);
+                        }
+                        await res.SendString(filename);
+                        uploadLogger.Log(LogLevel.Info,
+                            $"{req.UnderlyingContext.Connection.RemoteIpAddress.ToString()}: {filename}");
+                    }
+                    catch (Exception e)
+                    {
+                        if (File.Exists(filepath))
+                        {
+                            File.Delete(filepath);
+                            Console.WriteLine("File deleted after failed upload: " + filename);
+                        }
+                        uploadLogger.Log(LogLevel.Error, e);
+                        
+                        await res.SendString("File upload failed", status: HttpStatusCode.InternalServerError);
+                    }
+                }
+                else
+                {
+                    await res.SendString($"The maximum allowed filesize is {maxSizeMiB}",
+                        status: HttpStatusCode.BadRequest);
+                }
+            });
+
+            server.Get("/download/:filename", async (req, res) =>
+            {
+                var filename = HttpUtility.UrlDecode(req.Parameters["filename"]);
+                var filepath = Path.Combine(UploadDirectory, Path.GetFileName(filename));
+
+                if (File.Exists(filepath))
+                {
+                    await res.Download(filepath, filename.Substring(7));
+                }
+                else
+                {
+                    await res.SendString("The file was not found. Perhaps it has been deleted",
+                        status: HttpStatusCode.NotFound);
+                }
+            });
+            
+            SetupLogging();
+            StartFileReaper();
+            await server.RunAsync();
+        }
+
+
+        public static string GetSizeString(long size)
+        {
+            if (size > DisplayKiBLimit)
+            {
+                return $"{size / 1024f / 1024f:F2} MiB";
+            }
+
+            return $"{size / 1024f:F1} KiB";
+        }
+
+        public static string FormatTime(DateTime uploadedUtc)
+        {
+            var uploaded = (int) DateTime.UtcNow.Subtract(uploadedUtc).TotalSeconds;
+            if (uploaded == 0)
+                return "Uploaded just now";
+            if (uploaded < 60)
+                return $"Uploaded {uploaded} seconds age";
+            if (uploaded == 1)
+                return "Uploaded 1 minute ago";
+            return $"Uploaded {uploaded / 60} minutes ago";
+        }
+
+        private static void SetupLogging()
+        {
+            var config = new NLog.Config.LoggingConfiguration();
+
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "LOG.txt" };
+            var logconsole = new NLog.Targets.ConsoleTarget("logconsole");
+            
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logconsole);
+            config.AddRule(LogLevel.Info, LogLevel.Fatal, logfile);
+            
+            LogManager.Configuration = config;
+        }
+
+        private static bool CheckSpaceConsumption(long uploadedFileSize)
+        {
+            var files = Directory.GetFiles(UploadDirectory);
+            long consumedSpace = files.Sum(file => new FileInfo(file).Length);
+            return (consumedSpace + uploadedFileSize) < MaxSpaceConsumption;
+        }
+
+        private static void StartFileReaper()
+        {
+            Task.Run(async () =>
+            {
+                while (true)
+                {
+                    ReapFiles();
+                    await Task.Delay(TimeSpan.FromSeconds(30));
+                }
+            });
+        }
+
+        private static void ReapFiles()
+        {
+            var files = Directory.EnumerateFiles(UploadDirectory);
+            var threshold = DateTime.UtcNow.Subtract(TimeSpan.FromMinutes(10));
+
+            Parallel.ForEach(files, file =>
+            {
+                var info = new FileInfo(file);
+                if (info.CreationTimeUtc < threshold)
+                {
+                    File.Delete(file);
+                }
+            });
+        }
+
+        private static string GenerateRandom(int length)
+        {
+            var sb = new StringBuilder();
+            var random = new Random();
+            var possibleChars = PossibleRandomCharacters.Length - 1;
+            for (int i = 0; i < length; i++)
+            {
+                sb.Append(PossibleRandomCharacters[random.Next(0, possibleChars)]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
